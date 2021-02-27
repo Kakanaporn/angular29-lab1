3bd8d28dce35c0cd6b024efaed2bb66b12d6b6fd
Kakanaporn <skakanaporn@gmail.com>
2021-02-27 19:39:58 +0700

    Recipe#1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular29-lab1';
+  homeTitleValue = `Set home's title from typescript`;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnChanges {
+  @Input()
+  title: string
+  constructor() { 
+    console.log(`[Constructor] - title value = ${this.title}`);
+  }
 
   ngOnInit(): void {
+    console.log(`[Constructor] - title value = ${this.title}`);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(`[Before] title=${changes.title.previousValue}`);
+    console.log(`[Current] title=${changes.title.currentValue}`);
   }
 
 }
